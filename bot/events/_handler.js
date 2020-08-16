6d08c794b03a30ffd43f5b6aa3145051897f795d
arc9999 <arcadexfr@gmail.com>
2020-08-16 14:46:45 +1000

    remove

--- bot/events/_handler.js
+++
@@ -1,7 +0,0 @@
-const event = (event) => require(`./${event}`);
-module.exports = client => {
-    client.on('ready', () => event('ready')(client));
-    client.on('message', event('message'));
-    client.on('guildCreate', event('join'));
-    client.on('guildUpdate', event('guildUpdate'));
-};
--- bot/events/message.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-const Discord = require('discord.js');
-const client = new Discord.Client();
-module.exports = message => {
-}
